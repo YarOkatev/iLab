459ffa24e3db8f64aa3c98a1e104eb753584e12e
YarOkatev <yar.ok@mail.ru>
2018-10-16 18:50:17 +0300

    Revert "fix realloc"
    This reverts commit 7dc507f0971f58884784fb8cea833fcb04122623.

--- Laba 1.1.4/laba.c
+++ Laba 1.1.4/laba.c
@@ -43,6 +43,7 @@ int ReadData (int n20[])
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 6c23918391b2b7965a576e4a485c972c0d3bc830
     if (point > arrSize - 1)
@@ -59,6 +60,9 @@ int ReadData (int n20[])
 >>>>>>> parent of 7dc507f... fix realloc
 =======
 >>>>>>> 6c23918391b2b7965a576e4a485c972c0d3bc830
+=======
+    if (point > DefaultNP - 1) n20 = (int*) realloc(n20, (point + 100) * sizeof(int));
+>>>>>>> parent of 7dc507f... fix realloc
 =======
     if (point > DefaultNP - 1) n20 = (int*) realloc(n20, (point + 100) * sizeof(int));
 >>>>>>> parent of 7dc507f... fix realloc
