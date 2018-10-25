2c521f392f09cf9777fdc4a5040a153fb1c973d2
YarOkatev <yar.ok@mail.ru>
2018-10-25 14:39:11 +0300

    23234

--- Onegin/evgeny.c
+++ Onegin/evgeny.c
@@ -9,20 +9,51 @@ typedef struct StrP
 } StringPlus;
 
 int FileRead ();
+void DataBeatify ();
+int FileCount ();
 
 int main ()
 {
   int nSymb = 0;
   StringPlus* text = NULL;
-  int RCheck = -1;
+  char* fcontent = (char*) calloc (1000, sizeof(char));
+  //int RCheck = -1;
+  FILE* poem;
 
-  RCheck = FileRead (text, &nSymb);
+  FileCount (poem, &nSymb);
+
+  FileRead (poem, fcontent, &nSymb);
+  printf("\n second %d \n%s\n", nSymb, fcontent);
+  DataBeatify (fcontent, nSymb, text);
   return 0;
 }
 
-int FileRead (StringPlus* text, int* nSymb)
+int FileRead (FILE* poem, char* fcontent, int* nSymb)
 {
-  FILE* poem = fopen ("poem.txt", "r"); // file in win (1251) or cp866
+  /* FILE* poem = fopen ("poem.txt", "r"); // file in win (1251) or cp866
+  if (!poem)
+  {
+    printf ("Can't open file \n");
+    return -1;
+  }
+  struct stat st = {};
+  stat ("poem.txt", &st);
+  *nSymb = (int)st.st_size; */
+  //fcontent = (char*) realloc (fcontent, (*nSymb + 1) * sizeof(char));
+  fread (fcontent, sizeof(char), *nSymb, poem);
+  fcontent[*nSymb] = '\0';
+  printf("\nfirst\n%s\n", fcontent);
+  return 0;
+}
+
+void DataBeatify (char* fcontent, int nSymb, StringPlus* text)
+{
+
+}
+
+int FileCount (FILE* poem, int* nSymb)
+{
+  poem = fopen ("poem.txt", "r"); // file in win (1251) or cp866
   if (!poem)
   {
     printf ("Can't open file \n");
@@ -31,10 +62,4 @@ int FileRead (StringPlus* text, int* nSymb)
   struct stat st = {};
   stat ("poem.txt", &st);
   *nSymb = (int)st.st_size;
-  //printf ("%lld \n", sizeof(char));
-  char* fcontent = (char*) calloc (*nSymb + 1, sizeof(char));
-  fread (fcontent, sizeof(char), *nSymb, poem);
-  fcontent[*nSymb] = '\0';
-  printf("%s\n", fcontent);
-  return 0;
 }
