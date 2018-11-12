15d815cd6b1da6dc28a90ad09e8aa7c7bce183d4
YarOkatev <yar.ok@mail.ru>
2018-11-12 22:56:44 +0300

    writing

--- Onegin/evgeny.c
+++ Onegin/evgeny.c
@@ -12,19 +12,20 @@ typedef struct StringPlus
 
 char* FileRead ();
 StrP* BetterData ();
+void Sorting ();
+int FileWrite ();
 
 int main ()
 {
-  int nSymb = 0;
+  int nSymb = 0, nStrings = 0;
   char* poem = NULL;
   StrP* text = NULL;
   poem = FileRead (&nSymb);
-  text = BetterData (poem, nSymb);
-  
-  for (int i = 0; i <= 15; i++)
-  {
-    printf("%d= %s\n",text[i].len, text[i].str);
-  }
+  text = BetterData (poem, nSymb, &nStrings);
+  //Sorting (text, poem);
+  FileWrite (text, poem, nStrings);
+
+  // printf("%c \n", text[i].str[text[i].len - 10]);
   return 0;
 }
 
@@ -34,7 +35,6 @@ char* FileRead (int* nSymb)
   if (!filepoem)
   {
     printf ("Can't open file \n");
-    fclose (filepoem);
     return NULL;
   }
   struct stat st = {};
@@ -46,10 +46,10 @@ char* FileRead (int* nSymb)
   return poem;
 }
 
-StrP* BetterData (char* poem, int nSymb)
+StrP* BetterData (char* poem, int nSymb, int* nStrings)
 {
   StrP* text = (StrP*) calloc (DefSN, sizeof(StrP));
-  int k = 0;
+  int k = 0, range = DefSN;
   text[k].str = poem;
   char* prev = poem - 1;
   for (int i = 0; i < nSymb; i++)
@@ -58,10 +58,38 @@ StrP* BetterData (char* poem, int nSymb)
     {
       text[k].len = poem + i - prev;
       k++;
-      poem[i] = '\0';
+      //poem[i] = '\0';
       text[k].str = poem + i + 1;
       prev = poem + i;
     }
+    if (k == range - 1)
+    {
+      text = (StrP*) realloc (text, (range + DefSN) * sizeof(StrP));
+      range += DefSN;
+    }
   }
+  text = (StrP*) realloc (text, (k + 1) * sizeof(StrP));
+  *nStrings = k + 1;
   return text;
+}
+
+// void Sorting (StrP* text, char* poem)
+// {
+//   qsort (text,
+// }
+
+int FileWrite (StrP* text, char* poem, int nStrings)
+{
+  FILE* output = fopen ("out.txt", "w");
+  if (!output)
+  {
+    printf ("Can't open file \n");
+    return -1;
+  }
+  for (int i = 0; i < nStrings; i++)
+  {
+    fwrite (text[i].str, sizeof(char), text[i].len, stdout);
+  }
+  fclose (output);
+  return 0;
 }
