c89c5ba827398fbfaa0bd2f98b26d030a6671acf
YarOkatev <yar.ok@mail.ru>
2018-11-26 18:56:59 +0300

    ya vsyo no nichego ne rabotaet

--- Nodes/node.c
+++ Nodes/node.c
@@ -6,39 +6,54 @@
 
 const int CAN = 31415;
 
-struct _node {
+struct _Node {
   int canary1;
   int val;
   int checksum;
-  struct _node* prev;
-  struct _node* next;
+  struct _Node* prev;
+  struct _Node* next;
   int canary2;
 };
 
-typedef struct _node node;
+struct _List {
+  int size;
+  struct _Node* head;
+  struct _Node* tail;
+};
 
-node* CreateNode (int val_, node* prev_, node* next_, int key);
-void ConnectNodes (node* left, node* right);
+typedef struct _List List;
+typedef struct _Node Node;
+
+Node* InitNode(int val_, Node* prev_, Node* next_, int key, List* list);
 int KeyGen ();
-void DeleteNode (node* del);
-void OutputList (node* start, int key);
+void DeleteNode (Node* del, List* list);
+void OutputList (List* out, int key);
+List* InitList ();
+void ConnectNodes (Node* left, Node* right);
+
+//------------------------------------------------//
 
 int main () {
   int key = KeyGen ();
-  node* first = CreateNode (rand () % 1000, NULL, NULL, key);
-  node* n = first;
+
+  List* first = InitList ();
+
+  Node* n = InitNode(rand () % 1000, NULL, NULL, key, first);
   for (int i = 0; i < 10; i++) {
-    n = CreateNode (rand () % 1000, n, NULL, key);
+    n = InitNode(rand () % 1000, n, NULL, key, first);
     ConnectNodes (n->prev, n);
   }
+
   //
   // char* a = (char*) calloc (1, sizeof(char));
   // for (int i = 0; i < 1000; i++)
   //   a[i] = 1;
   // printf("\n");
 
-  OutputList (first->next, key);
+  OutputList (first, key);
 }
+
+//------------------------------------------------//
 
 int KeyGen () {
   int seed = time (NULL);
@@ -47,43 +62,72 @@ int KeyGen () {
   return key;
 }
 
-node* CreateNode (int val_, node* prev_, node* next_, int key) {
-  // assert (prev == next);
-  // assert (!prev);
-  // assert (!next);
-  node* ret = (node*) calloc (1, sizeof(node));
+Node* InitNode(int val_, Node* prev_, Node* next_, int key, List* list) {
+  Node* ret = (Node*) calloc (1, sizeof(Node));
   ret->canary1 = CAN;
   ret->val = val_;
   ret->checksum = val_ ^ key;
   ret->prev = prev_;
   ret->next = next_;
   ret->canary2 = CAN;
+  if (prev_ == NULL)
+    list->tail = ret;
+  if (next_ == NULL)
+    list->head = ret;
+  list->size -= 1;
   return ret;
 }
 
-void DeleteNode (node* del) {
-  assert (!del);
-  del->prev->next = del->next;
-  del->next->prev = del->prev;
+void DeleteNode (Node* del, List* list) {
+  if (!del) {
+    del->prev->next = del->next;
+    del->next->prev = del->prev;
+  }
+  else {
+    printf("Deleting error\n");
+  }
+  if (del->next == NULL)
+    list->head = del->prev;
+  if (del->prev == NULL)
+    list->tail = del->next;
+  list->size -= 1;
+  free (del);
+  del = NULL;
 }
 
-void ConnectNodes (node* left, node* right) {
+void ConnectNodes (Node* left, Node* right) {
   // assert (!left);
   // assert (!right);
   // assert (left->prev == right);
   // assert (right->next == left);
   //assert (left == right);
-  right->prev = left;
-  left->next = right;
+    right->prev = left;
+    left->next = right;
 }
 
-void OutputList (node* start, int key) {
-  if (start) {
-    if (((start->val ^ key) == start->checksum) && (start->canary1 == CAN) && (start->canary2 == CAN)) {
-      printf("%d\n", start->val);
-      OutputList (start->next, key);
-    }
-    else
-    printf("Unfortunately, your data lost :(\n");
+void OutputList (List* out, int key) {
+  int i = out->size;
+  if (i == 0) {
+    printf("List is empty\n");
+    return;
   }
+  Node* tmp = out->tail;
+  for (; i >= 0; i--) {
+    if (((tmp->val ^ key) == tmp->checksum) && (tmp->canary1 == CAN) && (tmp->canary2 == CAN)) {
+      printf("%d\n", tmp->val);
+    }
+    else {
+      printf("Unfortunately, your data lost :(\n");
+      break;
+    }
+    tmp = tmp->next;
+  }
+}
+
+List* InitList () {
+  List* tmp = (List*) calloc (1, sizeof(List));
+  tmp->size = 0;
+  tmp->head = NULL;
+  tmp->tail = NULL;
+  return tmp;
 }
