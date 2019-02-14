812a3dc397e72870e7eaecb8c4241c8f89a7714f
YarOkatev <yar.ok@mail.ru>
2019-02-14 14:38:56 +0300

    akinator final

--- Akinator 2.0/main.cpp
+++ Akinator 2.0/main.cpp
@@ -1,11 +1,11 @@
 #include "akinator.h"
 
 int main () {
-    FILE* file = fopen ("2.db", "r");
+    char* fileName = const_cast<char*>("data.db");
+    FILE* file = fopen (fileName, "r");
     Node* tree = readDB (file);
-    fclose (file);
     play (tree);
-    file = fopen ("2.db", "w");
+    file = fopen (fileName, "w");
     writeDB (file, tree);
     fclose (file);
     removeTree (tree);
